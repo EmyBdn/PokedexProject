17fff2caf4b4d4f7e7686724260d81be2398c8f2
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-13 18:11:05 +0200

    feat(app): setup App component with routing and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import {BrowserRouter, Route, Routes} from "react-router";
+import {ROUTES} from "./routes/Routes.jsx";
+import {AppLayout} from "./components/AppLayout/AppLayout.jsx";
+import {HomePage} from "./pages/HomePage/HomePage.jsx";
+import {Details} from "./pages/Details/Details.jsx";
+import {NotFound} from "./pages/NotFound/NotFound.jsx";
 
 export function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={ROUTES.HOME} element={<AppLayout/>}>
+                        <Route path={ROUTES.HOME} element={<HomePage/>}/>
+                        <Route path={ROUTES.DETAILS} element={<Details/>}/>
+                    </Route>
+                    <Route path={ROUTES.NOTFOUND} element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
