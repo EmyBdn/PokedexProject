c1eeb4d6373d734cc1689a424181fc76a14301a5
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-13 18:04:03 +0200

    feat(constants): add constants for image paths

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,3 +1,6 @@
 export const CONSTANTS = {
+    LOGO:'../src/assets/images/pokedex_logo.png',
+    POKEBALL_ICON: '../src/assets/images/pokeball_icon.png',
+    POKEBALL_BACKGROUND:'./src/assets/images/pokeball_bg.png',
 
 }
