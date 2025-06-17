9b7a2daabcd67369fc986e85219dbe31c55ef551
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-17 12:18:50 +0200

    feat(api) : fetch all pokemons data

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,1 +1,13 @@
-//Fonctions pour fetch la data
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const allPokemons = import.meta.env.VITE_GET_ALL_POKEMONS;
+
+
+export async function getPokemonList({setPokemonList}) {
+    try {
+        const response = await fetch(`${baseUrl}${allPokemons}`);
+        const data = await response.json();
+        setPokemonList(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
