51f6927bcb7383ad856889c5024b0983df457580
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-17 12:23:35 +0200

    feat(homepage) : create component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,31 @@
+import {useEffect, useState} from "react";
+import {getPokemonList} from "../../api/api.jsx";
+import {PokemonList} from "../../components/PokemonList/PokemonList.jsx";
+import {SearchBar} from "../../components/SearchBar/SearchBar.jsx";
+import { filterPokemons } from "/src/utils/utils.jsx"
+import style from "./HomePage.module.css"
+
 export function HomePage() {
+    const [pokemonList, setPokemonList] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        getPokemonList({setPokemonList});
+    }, [])
+
+    const filteredPokemons = filterPokemons(pokemonList, search);
+
     return (
-        <>
-            <h1>
-                HomePage
-            </h1>
-            <p>Welcome to the greatest Pokédex ever !</p>
-        </>
+        <div className={style.homeContainer}>
+            <SearchBar value={search} onChange={setSearch} />
+
+            {filteredPokemons.length === 0 && search.trim() !== "" ? (
+                <p>
+                    No results found for "{search}"
+                </p>
+            ) : (
+                <PokemonList pokemonList={filteredPokemons} />
+            )}
+        </div>
     )
 }
