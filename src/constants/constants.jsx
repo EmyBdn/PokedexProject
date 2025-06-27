19f2758a81a406fa1fee6ea424992df9092a7d03
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-28 00:42:12 +0200

    chore(constants) : update img path

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,7 +1,7 @@
 export const CONSTANTS = {
     LOGO:'../src/assets/images/pokedex_logo.png',
     POKEBALL_ICON: '../src/assets/images/pokeball_icon.png',
-    POKEBALL_BACKGROUND:'./src/assets/images/pokeball_bg.png',
+    POKEBALL_BACKGROUND:'/assets/images/pokeball_bg.svg',
 
     POKEMON_IMG:'http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world'
 
