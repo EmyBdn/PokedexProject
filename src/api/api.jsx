8834ec72c428319899b7f3b14ea9a89e0370c03b
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-28 00:40:58 +0200

    feat(api) : adding data fetching & post functions

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,7 @@
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const allPokemons = import.meta.env.VITE_GET_ALL_POKEMONS;
+const onePokemon = import.meta.env.VITE_GET_ONE_POKEMON;
+const reviews = import.meta.env.VITE_GET_REVIEWS;
 
 
 export async function getPokemonList({setPokemonList}) {
@@ -9,5 +11,45 @@ export async function getPokemonList({setPokemonList}) {
         setPokemonList(data);
     } catch (error) {
         console.error(error);
+    }
+}
+
+export async function getPokemonById({id, setPokemon}) {
+    try {
+        const response = await fetch(`${baseUrl}${onePokemon.replace(":id", id)}`);
+        const data = await response.json();
+        setPokemon(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getReviewsList({id, setReviewsList}) {
+    try {
+        const response = await fetch(`${baseUrl}${reviews.replace(":id", id)}`);
+        const data = await response.json();
+        setReviewsList(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function postReview({ review, onSuccess }) {
+    try {
+        const response = await fetch(`${baseUrl}/reviews/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(review)
+        });
+
+        if (!response.ok) {
+            throw new Error("Erreur lors de la création de la review");
+        }
+
+        if (onSuccess) onSuccess();
+    } catch (error) {
+        console.error("Erreur postReview:", error);
     }
 }
