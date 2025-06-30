6f4ffa713e5538bd72609479be3d552292a3ec12
Emeline Baudouin <emeline.baudouin.cefim@gmail.com>
2025-06-30 17:40:41 +0200

    feat(Details): add background, likesCounter and navigation buttons to Details page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,13 +1,15 @@
 import style from '/src/pages/Details/Details.module.css';
 import {useState, useEffect} from 'react';
 import {useParams} from "react-router";
-import {getPokemonById, getReviewsList} from "../../api/api.jsx";
+import {getPokemonById, getReviewsList, updateLikesCount} from "../../api/api.jsx";
 import {CONSTANTS} from "../../constants/constants.jsx";
 import {ReviewsList} from "../../components/Reviews/ReviewsList/ReviewsList.jsx";
 import {ReviewInput} from "../../components/Reviews/ReviewInput/ReviewInput.jsx";
 import {postReview} from "../../api/api.jsx";
 import {Link} from "react-router";
 import {PokemonStatsList} from "../../components/PokemonStats/PokemonStatsList/PokemonStatsList.jsx";
+import {PokemonTypeBadge} from "../../components/PokemonStats/PokemonTypeBadge/PokemonTypeBadge.jsx";
+import {FaArrowLeft, FaArrowRight} from "react-icons/fa";
 
 export function Details() {
     const MAX_ID = 151;
@@ -40,28 +42,41 @@ export function Details() {
         });
     }
 
+    function handleLike() {
+        const updatedLike = pokemon.like + 1;
+
+        updateLikesCount({id, newLikeCount: updatedLike}).then((updatedData) => {
+            setPokemon(prev => ({...prev, like: updatedData.like}));
+        });
+    }
 
     return (
-        <>
+        <div className={style.detailsPageContainer} style={{
+            backgroundImage: `url(${CONSTANTS.POKEBALL_BACKGROUND})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+        }}>
             <div className={style.navigationButtons}>
-                {previousId &&
-                    (<Link to={`/pokemon/${previousId}`}>
-                            <button>Previous</button>
-                        </Link>
-                    )}
-                {nextId &&
-                    (<Link to={`/pokemon/${nextId}`}>
-                        <button>Next</button>
-                    </Link>)
-                }
+                {previousId ? (
+                    <Link to={`/pokemon/${previousId}`}>
+                        <FaArrowLeft className={style.arrow}/>
+                    </Link>
+                ) : (
+                    <div className={style.arrowPlaceholder}/>
+                )}
+
+                {nextId ? (
+                    <Link to={`/pokemon/${nextId}`}>
+                        <FaArrowRight className={style.arrow}/>
+                    </Link>
+                ) : (
+                    <div className={style.arrowPlaceholder}/>
+                )}
             </div>
 
-            <div className={style.detailsContainer}
-                 style={{
-                     backgroundImage: `url(${CONSTANTS.POKEBALL_BACKGROUND})`,
-                     backgroundPosition: "center",
-                     backgroundRepeat: "no-repeat",
-                 }}>
+
+            <div className={style.detailsContainer}>
 
                 <div className={style.illustrationContainer}>
                     {pokemon && <img className={style.illustration}
@@ -70,16 +85,26 @@ export function Details() {
                     />}
                 </div>
 
-                <div className={style.statsContainer}>
-                    {pokemon && <PokemonStatsList pokemon={pokemon}/>}
-                </div>
+                {pokemon && <div className={style.infosContainer}>
+                    <h1>{pokemon.name}</h1>
+                    <PokemonTypeBadge types={pokemon.types}/>
+                    <PokemonStatsList base={pokemon.base}/>
+                </div>}
 
                 <div className={style.reviewsContainer}>
-                    <h1>Reviews</h1>
+
+                    <div className={style.reviewsHeader}>
+                        {pokemon && <div className={style.likesContainer}>
+                        <button onClick={handleLike}>♥️</button>
+                        <p>{pokemon.like}</p>
+                    </div>}
+                        <h1>Reviews</h1>
+                    </div>
+
                     <ReviewInput onSubmit={handleAddReview}/>
                     <ReviewsList reviewsList={reviewsList}/>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
